a21d3ed55e4142100fa0d86d6d5d7de2e7ba36c4
Eligarf <eligarf@live.com>
2025-01-20 22:28:47 -0800

    hitting it with a stick

--- esmodules/StartLogging.js
+++ esmodules/StartLogging.js
@@ -1,3 +1,5 @@
+import { MODULE_ID, log } from "./main";
+
 const TABLE = ['unknown', 'criticalFailure', 'failure', 'success', 'criticalSuccess'];
 
 const SESSION_UUID = foundry.utils.randomID();
@@ -14,66 +16,68 @@ async function recordRoll({ id, type, roller, vs = null, d20, needs, dos }) {
   const update = {
     _id: game.user.id,
   };
-  update[`flags.confirm-bias.sessions.${SESSION_UUID}.${rollerId}.${d20}.${dos}.${type}.${id}`] = { vs: vsId, needed: needs };
+  update[`flags.${MODULE_ID}.sessions.${SESSION_UUID}.${rollerId}.${d20}.${dos}.${type}.${id}`] = { vs: vsId, needed: needs };
   await User.updateDocuments([update]);
 }
 
-Hooks.on('createChatMessage', async (message, options, id) => {
-  const roll = message.rolls[0];
-  const rollOpt = roll?.options;
-  const type = rollOpt?.type;
-  if (!['attack-roll', 'skill-check', 'initiative', 'saving-throw'].includes(type)) {
-    return;
-  }
-
-  // console.log('createChatMessage', { message, roll, type });
-  const roller = game.users.find(u => u.character?.id === message.flags.pf2e.context.actor)?.id;
-  const d20 = roll.dice[0].total;
-  const dos = TABLE[1 + (rollOpt?.degreeOfSuccess ?? -1)];
-  const modifier = rollOpt.totalModifier;
-  const context = message.flags.pf2e.context;
-  const targetToken = context?.target?.token ? await fromUuid(context?.target?.token) : null;
-  const vs = targetToken ? game.users.find((u) => u.character?.id === targetToken.actor?.id)?.id : null;
-  const dc = context.dc?.value;
-  const isReroll = context.isReroll;
-  const needs = dc - modifier;
-  recordRoll({ id: message.id, type, roller, vs, d20, dos, needs, isReroll });
-});
-
-Hooks.on('updateChatMessage', async (message, delta, options, id) => {
-  const toolbelt = delta?.flags['pf2e-toolbelt'];
-  // console.log('updateChatMessage', { message, delta });
-
-  if (toolbelt) {
-    const saves = toolbelt?.targetHelper?.saves;
-    const actor = message?.flags?.pf2e?.origin?.actor ? await fromUuid(message?.flags?.pf2e?.origin?.actor) : null;
-    const vs = game.users.find(u => u.character === actor)?.id;
-    for (const tokenId in saves) {
-      const token = canvas.scene.tokens.get(tokenId);
-      const roller = token ? game.users.find((u) => u.character?.id === token?.actor?.id)?.id : null;
-      const save = saves[tokenId];
-      const d20 = save.die;
-      const dos = save.success;
-      const modifier = save.value - save.die;
-      const dc = message.flags['pf2e-toolbelt']?.targetHelper?.save?.dc;
-      const needs = dc - modifier;
-      const isReroll = save?.rerolled === 'new';
-      recordRoll({ id: message.id, type: 'saving-throw', roller, vs, d20, dos, needs, isReroll });
+Hooks.once('init', () => {
+  Hooks.on('createChatMessage', async (message, options, id) => {
+    const roll = message.rolls[0];
+    const rollOpt = roll?.options;
+    const type = rollOpt?.type;
+    if (!['attack-roll', 'skill-check', 'initiative', 'saving-throw'].includes(type)) {
+      return;
     }
-    return;
-  }
-  const roll = delta?.flags?.['pf2e-flatcheck-helper']?.flatchecks?.targets;
-  if (!roll) return;
-  const flatcheck = message.flags['pf2e-flatcheck-helper'].flatchecks.targets;
-  const actor = message?.flags?.pf2e?.origin?.actor ? await fromUuid(message?.flags?.pf2e?.origin?.actor) : null;
-  recordRoll({
-    id: message.id,
-    type: 'flatcheck',
-    d20: flatcheck.roll,
-    needs: flatcheck.dc,
-    roller: game.users.find(u => u.character?.id === actor?.id)?.id,
-    dos: (flatcheck.roll >= flatcheck.dc) ? 'success' : 'failure',
-  });
-});
 
-ui.notifications.notify(`Logging session ${SESSION_UUID}`);
+    // log('createChatMessage', { message, roll, type });
+    const roller = game.users.find(u => u.character?.id === message.flags.pf2e.context.actor)?.id;
+    const d20 = roll.dice[0].total;
+    const dos = TABLE[1 + (rollOpt?.degreeOfSuccess ?? -1)];
+    const modifier = rollOpt.totalModifier;
+    const context = message.flags.pf2e.context;
+    const targetToken = context?.target?.token ? await fromUuid(context?.target?.token) : null;
+    const vs = targetToken ? game.users.find((u) => u.character?.id === targetToken.actor?.id)?.id : null;
+    const dc = context.dc?.value;
+    const isReroll = context.isReroll;
+    const needs = dc - modifier;
+    await recordRoll({ id: message.id, type, roller, vs, d20, dos, needs, isReroll });
+  });
+
+  Hooks.on('updateChatMessage', async (message, delta, options, id) => {
+    const toolbelt = delta?.flags['pf2e-toolbelt'];
+    // log('updateChatMessage', { message, delta });
+
+    if (toolbelt) {
+      const saves = toolbelt?.targetHelper?.saves;
+      const actor = message?.flags?.pf2e?.origin?.actor ? await fromUuid(message?.flags?.pf2e?.origin?.actor) : null;
+      const vs = game.users.find(u => u.character === actor)?.id;
+      for (const tokenId in saves) {
+        const token = canvas.scene.tokens.get(tokenId);
+        const roller = token ? game.users.find((u) => u.character?.id === token?.actor?.id)?.id : null;
+        const save = saves[tokenId];
+        const d20 = save.die;
+        const dos = save.success;
+        const modifier = save.value - save.die;
+        const dc = message.flags['pf2e-toolbelt']?.targetHelper?.save?.dc;
+        const needs = dc - modifier;
+        const isReroll = save?.rerolled === 'new';
+        await recordRoll({ id: message.id, type: 'saving-throw', roller, vs, d20, dos, needs, isReroll });
+      }
+      return;
+    }
+    const roll = delta?.flags?.['pf2e-flatcheck-helper']?.flatchecks?.targets;
+    if (!roll) return;
+    const flatcheck = message.flags['pf2e-flatcheck-helper'].flatchecks.targets;
+    const actor = message?.flags?.pf2e?.origin?.actor ? await fromUuid(message?.flags?.pf2e?.origin?.actor) : null;
+    await recordRoll({
+      id: message.id,
+      type: 'flatcheck',
+      d20: flatcheck.roll,
+      needs: flatcheck.dc,
+      roller: game.users.find(u => u.character?.id === actor?.id)?.id,
+      dos: (flatcheck.roll >= flatcheck.dc) ? 'success' : 'failure',
+    });
+  });
+
+  ui.notifications.notify(`Logging session ${SESSION_UUID}`);
+});
--- esmodules/main.js
+++ esmodules/main.js
@@ -1,8 +1,5 @@
-const CONSOLE_COLORS = ['background: #222; color: #80ffff', 'color: #fff'];
-const SKILL_ACTIONS = ['action:hide', 'action:create-a-diversion', 'action:sneak'];
-const MODULE_ID = 'pf2e-avoid-notice';
-const PF2E_PERCEPTION_ID = 'pf2e-perception';
-const PERCEPTIVE_ID = 'perceptive';
+const CONSOLE_COLORS = ['background: #222; color: #ffff80', 'color: #fff'];
+const MODULE_ID = 'pf2e-confirm-my-bias';
 
 function colorizeOutput(format, ...args) {
   return [
@@ -24,27 +21,7 @@ function log(format, ...args) {
   }
 }
 
-function getPerceptionApi() {
-  return game.modules.get(PF2E_PERCEPTION_ID)?.api;
-}
-
-function getPerceptiveApi() {
-  return game.modules.get(PERCEPTIVE_ID)?.api;
-}
-
-export { MODULE_ID, PF2E_PERCEPTION_ID, PERCEPTIVE_ID, log, getPerceptionApi, getPerceptiveApi };
-
-async function clearPerceptionData(token) {
-  // Remove any ids that perception is tracking
-  const perceptionData = token.flags?.[PF2E_PERCEPTION_ID]?.data;
-  if (!perceptionData || !Object.keys(perceptionData).length) return;
-  let tokenUpdate = {};
-  for (let id in perceptionData) {
-    tokenUpdate[`flags.${PF2E_PERCEPTION_ID}.data.-=${id}`] = true;
-  }
-  const updates = [{ _id: token.id, ...tokenUpdate }];
-  await canvas.scene.updateEmbeddedDocuments("Token", updates);
-}
+export { MODULE_ID, log };
 
 Hooks.once('init', () => {
   Hooks.on('createChatMessage', async (message, options, id) => {
@@ -88,136 +65,13 @@ Hooks.once('init', () => {
 
 function migrate(moduleVersion, oldVersion) {
 
-  ui.notifications.warn(`Updated PF2e Avoid Notice data from ${oldVersion} to ${moduleVersion}`);
+  ui.notifications.warn(`Updated PF2e Confirm my Bias data from ${oldVersion} to ${moduleVersion}`);
   return moduleVersion;
 }
-
-Hooks.once('ready', () => {
-
-  // Handle perceptive or perception module getting yoinked
-  const conditionHandler = game.settings.get(MODULE_ID, 'conditionHandler');
-  if (conditionHandler === 'perception' && !game.modules.get(PF2E_PERCEPTION_ID)?.active
-    || conditionHandler === 'perceptive' && !game.modules.get(PERCEPTIVE_ID)?.active
-  ) {
-    log(`resetting condition Handler from '${conditionHandler}' to 'ignore'`);
-    game.settings.set(MODULE_ID, 'conditionHandler', 'ignore');
-  }
-
-  async function clearPf2ePerceptionFlags(item, options, userId) {
-    // Only do stuff if we are changing hidden, undetected, or unnoticed conditions and using pf2e-perception
-    const conditionHandler = game.settings.get(MODULE_ID, 'conditionHandler');
-    if (conditionHandler !== 'perception') return;
-    const perceptionApi = getPerceptionApi();
-    if (!perceptionApi) return;
-    if (item?.type !== 'condition' || !['hidden', 'undetected', 'unnoticed'].includes(item?.system?.slug)) return;
-
-    // Get the token on the current scene
-    const token = options.parent?.parent ?? canvas.scene.tokens.find((t) => t.actorId === options.parent.id);
-    if (!token) return;
-    await clearPerceptionData(token);
-  }
-
-  if (game.modules.get(PF2E_PERCEPTION_ID)?.active) {
-    Hooks.on("deleteItem", async (item, options, userId) => {
-      await clearPf2ePerceptionFlags(item, options, userId);
-    });
-
-    Hooks.on("createItem", async (item, options, userId) => {
-      await clearPf2ePerceptionFlags(item, options, userId);
-    });
-  }
-
-});
 
 Hooks.once('setup', () => {
   const module = game.modules.get(MODULE_ID);
   const moduleVersion = module.version;
-
-  game.settings.register(MODULE_ID, 'useUnnoticed', {
-    name: game.i18n.localize(`${MODULE_ID}.useUnnoticed.name`),
-    hint: game.i18n.localize(`${MODULE_ID}.useUnnoticed.hint`),
-    scope: 'world',
-    config: true,
-    type: Boolean,
-    default: true,
-  });
-
-  game.settings.register(MODULE_ID, 'removeGmHidden', {
-    name: game.i18n.localize(`${MODULE_ID}.removeGmHidden.name`),
-    hint: game.i18n.localize(`${MODULE_ID}.removeGmHidden.hint`),
-    scope: 'world',
-    config: true,
-    type: Boolean,
-    default: false,
-  });
-
-  game.settings.register(MODULE_ID, 'requireActivity', {
-    name: game.i18n.localize(`${MODULE_ID}.requireActivity.name`),
-    hint: game.i18n.localize(`${MODULE_ID}.requireActivity.hint`),
-    scope: 'world',
-    config: true,
-    type: Boolean,
-    default: true,
-  });
-
-  const perception = game.modules.get(PF2E_PERCEPTION_ID)?.active;
-  const perceptive = game.modules.get(PERCEPTIVE_ID)?.active;
-
-  let choices = {
-    'ignore': `${MODULE_ID}.conditionHandler.ignore`,
-    'best': `${MODULE_ID}.conditionHandler.best`,
-    'worst': `${MODULE_ID}.conditionHandler.worst`,
-  };
-  if (perception) choices.perception = `${MODULE_ID}.conditionHandler.perception`;
-  if (perceptive) choices.perceptive = `${MODULE_ID}.conditionHandler.perceptive`;
-
-  game.settings.register(MODULE_ID, 'conditionHandler', {
-    name: game.i18n.localize(`${MODULE_ID}.conditionHandler.name`),
-    hint: game.i18n.localize(`${MODULE_ID}.conditionHandler.hint`),
-    scope: 'world',
-    config: true,
-    type: String,
-    choices,
-    default: (perception) ? 'perception' : 'ignore'
-  });
-
-  if (perception) {
-    game.settings.register(MODULE_ID, 'computeCover', {
-      name: game.i18n.localize(`${MODULE_ID}.computeCover.name`),
-      hint: game.i18n.localize(`${MODULE_ID}.computeCover.hint`),
-      scope: 'world',
-      config: perception,
-      type: Boolean,
-      default: false,
-    });
-  }
-
-  game.settings.register(MODULE_ID, 'raiseShields', {
-    name: game.i18n.localize(`${MODULE_ID}.raiseShields.name`),
-    hint: game.i18n.localize(`${MODULE_ID}.raiseShields.hint`),
-    scope: 'world',
-    config: true,
-    type: Boolean,
-    default: true,
-  });
-
-  game.settings.register(MODULE_ID, 'autorollSpellDamage', {
-    name: game.i18n.localize(`${MODULE_ID}.autorollSpellDamage.name`),
-    hint: game.i18n.localize(`${MODULE_ID}.autorollSpellDamage.hint`),
-    scope: 'world',
-    config: true,
-    type: Boolean,
-    default: false,
-  });
-
-  game.settings.register(MODULE_ID, 'rage', {
-    name: game.i18n.localize(`${MODULE_ID}.rage.name`),
-    hint: game.i18n.localize(`${MODULE_ID}.rage.hint`),
-    scope: 'world',
-    config: true,
-    type: Boolean,
-    default: false,
-  });
 
   game.settings.register(MODULE_ID, 'logLevel', {
     name: game.i18n.localize(`${MODULE_ID}.logLevel.name`),
@@ -258,9 +112,7 @@ Hooks.once('setup', () => {
 
 Hooks.on('renderSettingsConfig', (app, html, data) => {
   const sections = [
-    { label: "general", before: "useUnnoticed" },
-    { label: "misfits", before: "raiseShields" },
-    { label: "debug", before: "logLevel" },
+     { label: "debug", before: "logLevel" },
   ];
   for (const section of sections) {
     $('<div>')
